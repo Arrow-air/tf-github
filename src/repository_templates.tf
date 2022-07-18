bdad0cb44a88206b852aadcf7430a566eb7e95e3
Owlot <95369289+owlot@users.noreply.github.com>
2022-07-18 20:27:36 +0200

    Added Python template repository

--- src/repository_templates.tf
+++ src/repository_templates.tf
@@ -1,3 +1,8 @@
+########################################################
+#
+# Rust template repository
+#
+########################################################
 resource "github_repository" "svc_template_rust" {
   name        = "svc-template-rust"
   description = "Arrow Service repository template for Rust services"
@@ -16,13 +21,44 @@ resource "github_team_repository" "svc_template_rust_maintainer" {
   permission = "maintain"
 }
 
-########################################################
-#
 # Provision sdd file
-#
-########################################################
 resource "github_repository_file" "svc_template_rust_sdd" {
   repository          = github_repository.svc_template_rust.name
+  branch              = "main"
+  file                = "docs/sdd.md"
+  content             = file("templates/svc/docs/sdd.md")
+  commit_message      = "Provisioned by Terraform"
+  commit_email        = "automation@arrowair.com"
+  commit_author       = "Arrow automation"
+  overwrite_on_create = false
+}
+
+########################################################
+#
+# Python template repository
+#
+########################################################
+resource "github_repository" "svc_template_python" {
+  name        = "svc-template-python"
+  description = "Arrow Service repository template for Python services"
+  visibility  = "public"
+
+  auto_init            = true
+  has_issues           = true
+  has_projects         = true
+  has_wiki             = true
+  vulnerability_alerts = true
+  is_template          = true
+}
+resource "github_team_repository" "svc_template_python_maintainer" {
+  repository = github_repository.svc_template_python.name
+  team_id    = "services"
+  permission = "maintain"
+}
+
+# Provision sdd file
+resource "github_repository_file" "svc_template_python_sdd" {
+  repository          = github_repository.svc_template_python.name
   branch              = "main"
   file                = "docs/sdd.md"
   content             = file("templates/svc/docs/sdd.md")
