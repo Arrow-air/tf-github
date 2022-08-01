e812ef248512278f0b9247cb5f28115beca848de
AM Smith <smitham42@gmail.com>
2022-08-01 09:42:29 -0700

    Added Rust Library template

--- src/repositories_templates.tf
+++ src/repositories_templates.tf
@@ -35,7 +35,7 @@ resource "github_repository_file" "svc_template_typescript_sdd" {
 
 ########################################################
 #
-# Rust template repository
+# Rust Service template repository
 #
 ########################################################
 resource "github_repository" "svc_template_rust" {
@@ -59,6 +59,41 @@ resource "github_team_repository" "svc_template_rust_maintainer" {
 # Provision sdd file
 resource "github_repository_file" "svc_template_rust_sdd" {
   repository          = github_repository.svc_template_rust.name
+  branch              = "main"
+  file                = "docs/sdd.md"
+  content             = file("templates/svc/docs/sdd.md")
+  commit_message      = "Provisioned by Terraform"
+  commit_email        = "automation@arrowair.com"
+  commit_author       = "Arrow automation"
+  overwrite_on_create = false
+}
+
+########################################################
+#
+# Rust Library template repository
+#
+########################################################
+resource "github_repository" "lib_template_rust" {
+  name        = "template-rust-lib"
+  description = "Arrow (Rust) Library Repository"
+  visibility  = "public"
+
+  auto_init            = true
+  has_issues           = true
+  has_projects         = true
+  has_wiki             = true
+  vulnerability_alerts = true
+  is_template          = true
+}
+resource "github_team_repository" "lib_template_rust_maintainer" {
+  repository = github_repository.lib_template_rust.name
+  team_id    = "services"
+  permission = "maintain"
+}
+
+# Provision sdd file
+resource "github_repository_file" "lib_template_rust_sdd" {
+  repository          = github_repository.lib_template_rust.name
   branch              = "main"
   file                = "docs/sdd.md"
   content             = file("templates/svc/docs/sdd.md")
