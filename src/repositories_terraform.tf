0f521b06dcc77bc953216df6cf40a0aadd4995a1
owlot <95369289+owlot@users.noreply.github.com>
2023-03-27 16:07:31 +0200

    feat(terraform): github actions fixes

--- src/repositories_terraform.tf
+++ src/repositories_terraform.tf
@@ -4,6 +4,55 @@
 #
 #####################################################################
 locals {
+  # Repositories needed for shared Terraform Modules can be listed here
+  tf_mod = {
+    files = local.terraform_default.files
+
+    # Using Terraform naming convention for modules
+    repos = {
+      "google-automation-setup" = {
+        description = "Automation resources"
+      }
+    }
+  }
+
+  # Repositories deploying infra to GCP
+  tf_gcp = {
+    template_files = {
+      ".env.base" = "templates/tf-gcp/.env.base.tftpl"
+      "Makefile"  = "templates/tf-gcp/Makefile"
+    }
+
+    files = {
+      ".github/workflows/terraform.yml" = {
+        content = file("templates/tf-gcp/.github/workflows/terraform.yml")
+      }
+      ".make/env.mk" = {
+        content = file("templates/all/.make/env.mk")
+      }
+    }
+
+    repos = {
+      "organization" = {
+        description = "GCP Organization management"
+        project     = "org"
+      }
+      "projects" = {
+        description = "GCP Project management"
+        project     = "org"
+      }
+      "network" = {
+        description = "GCP Network management"
+        project     = "network"
+      }
+      "services-app" = {
+        description = "GCP Services App infra management"
+        project     = "app"
+        owner_team  = "services"
+      }
+    }
+  }
+
   tf = {
     repos = {
       "onboarding" = {
@@ -25,44 +74,20 @@ locals {
           }
         }
       }
-      "gcp-organization" = {
-        description = "GCP Organization management"
-        project     = "org"
-      }
-      "gcp-projects" = {
-        description = "GCP Project management"
-        project     = "org"
-      }
-      "gcp-network" = {
-        description = "GCP Network management"
-        project     = "network"
-      }
-      "gcp-services-app" = {
-        description = "GCP Services App infra management"
-        project     = "app"
-        owner_team  = "services"
-      }
     }
   }
 
   terraform_default = {
     template_files = merge(local.template_files, {
-      ".env.base" = "templates/tf-all/.env.base.tftpl"
-      "Makefile"  = "templates/tf-all/Makefile"
+      "Makefile" = "templates/tf-all/Makefile"
     })
 
     files = merge(local.files, {
-      ".github/workflows/terraform.yml" = {
-        content = file("templates/tf-all/.github/workflows/terraform.yml")
-      }
       ".gitignore" = {
         content = file("templates/tf-all/.gitignore")
       }
       ".make/terraform.mk" = {
         content = file("templates/all/.make/terraform.mk")
-      }
-      ".make/env.mk" = {
-        content = file("templates/all/.make/env.mk")
       }
     })
 
@@ -103,6 +128,79 @@ module "repository_tf" {
     try(each.value.files, {}),
     { for file, path in local.terraform_default.template_files :
       file => { content = templatefile(path, { owner_team = each.value.owner_team, name = format("tf-%s", each.key), project = each.value.project }) }
+    }
+  )
+
+  collaborators = {
+    maintainers = ["services"]
+  }
+
+  default_branch_protection_settings = each.value.default_branch_protection_settings
+}
+
+########################################################
+# GCP Terraform repositories
+########################################################
+module "repository_tf_gcp" {
+  source   = "./modules/github-repository/"
+  for_each = { for key, settings in local.tf_gcp.repos : key => merge(local.terraform_default.settings, settings) }
+
+  name        = format("tf-gcp-%s", each.key)
+  description = format("Arrow Terraform - %s", each.value.description)
+
+  # Settings with defaults
+  owner_team            = each.value.owner_team
+  visibility            = each.value.visibility
+  default_branch        = each.value.default_branch
+  webhooks              = each.value.webhooks
+  terraform_app_node_id = local.arrow_release_automation_node_id
+
+  repository_files = merge(
+    local.terraform_default.files,
+    try(local.tf_gcp.files, {}),
+    try(each.value.files, {}),
+    { for file, path in merge(local.terraform_default.template_files, try(local.tf_gcp.template_files, {})) :
+      file => { content = templatefile(path, {
+        owner_team = each.value.owner_team
+        name       = format("tf-gcp-%s", each.key)
+        project    = each.value.project
+      }) }
+    }
+  )
+
+  collaborators = {
+    maintainers = ["services"]
+  }
+
+  default_branch_protection_settings = each.value.default_branch_protection_settings
+}
+
+########################################################
+# Terraform module repositories
+########################################################
+module "repository_tf_mod" {
+  source   = "./modules/github-repository/"
+  for_each = { for key, settings in local.tf_mod.repos : key => merge(local.terraform_default.settings, settings) }
+
+  name        = format("terraform-%s", each.key)
+  description = format("Arrow Terraform module - %s", each.value.description)
+
+  # Settings with defaults
+  owner_team            = each.value.owner_team
+  visibility            = each.value.visibility
+  default_branch        = each.value.default_branch
+  webhooks              = each.value.webhooks
+  terraform_app_node_id = local.arrow_release_automation_node_id
+
+  repository_files = merge(
+    local.terraform_default.files,
+    try(local.tf_mod.files, {}),
+    try(each.value.files, {}),
+    { for file, path in merge(local.terraform_default.template_files, try(local.tf_mod.template_files, {})) :
+      file => { content = templatefile(path, {
+        owner_team = each.value.owner_team
+        name       = format("terraform-%s", each.key)
+      }) }
     }
   )
 
