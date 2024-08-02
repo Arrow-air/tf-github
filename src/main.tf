e4bf436a9f7db2c8f2d4b289d9893314155ba4f1
Kellan Cerveny <kjcerveny@gmail.com>
2024-08-02 11:43:58 -0500

    feat: add 1 template for pods, 2 repos
    Attempting to add a template repo for future pod projects, and two new repos.
    1) project template
    2) dao-gbc
    3) dao-governance
    
    + Fix naming of files, repos
    + Fix pod naming change

--- src/main.tf
+++ src/main.tf
@@ -108,6 +108,18 @@ locals {
         maintainers = ["services", "drone-engineering"]
       }
     }
+    "dao-gbc" = {
+      default_branch = "main"
+      description    = "Arrowair Grants and Bounties Committee"
+      visibility     = "public"
+      owner_team     = "services"
+    }
+    "dao-governance" = {
+      default_branch = "main"
+      description    = "Arrowair DAO governance"
+      visibility     = "public"
+      owner_team     = "services"
+    }
   }
 }
 
